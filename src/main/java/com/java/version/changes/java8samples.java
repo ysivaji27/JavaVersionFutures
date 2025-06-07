5a636668309aba3c094a77a369424ae63eef052a
Sivaji Yelugoti <sivaji.y@gmail.com>
2025-06-07 19:30:13 +0100

    Update java8samples.java

--- src/main/java/com/java/version/changes/java8samples.java
+++ src/main/java/com/java/version/changes/java8samples.java
@@ -1,4 +1,13 @@
 package com.java.version.changes;
 
 public class java8samples {
+
+  // Lambda Expressions 
+  // Stream API
+  // Default Methods in the interface
+  // Static Methods 
+  // Functional Interfaces 
+  // Optional 
+  // Method References 
+  // Date API 
 }
