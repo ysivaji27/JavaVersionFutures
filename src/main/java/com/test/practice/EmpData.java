f06dc0067033effdc6427f95c4687dc160efc943
ysivaji27 <sivaji.y@gmail.com>
2025-06-30 20:37:07 +0100

    Added  hashmap with max and frequency

---
+++ src/main/java/com/test/practice/EmpData.java
@@ -0,0 +1,42 @@
+package com.test.practice;
+
+import java.util.List;
+
+public class EmpData {
+    int empId;
+    List<String> addressList;
+    String name;
+    String department;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public List<String> getAddressList() {
+        return addressList;
+    }
+
+    public void setAddressList(List<String> addressList) {
+        this.addressList = addressList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
